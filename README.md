1ce4c4c4da7aa3a77e8fb7df7dabcee8ffe1bfd6
Ilya Besancon <ilya.besancon@students.olin.edu>
2017-04-24 01:21:23 -0400

    Pushing latest version for MP5

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ComputationalArt
 This is the base repository for the computational art mini project.
+
+The images created using this program are located in the Artistic Creations folder.
+
+Enjoy!
--- recursive_art.py
+++ recursive_art.py
@@ -1,83 +1,45 @@
 """ @ilya-besancon
 This is the updated version of the computational art program.
+I modified my code to implement lambda values.
 """
 
 import random
 import math
 from PIL import Image
 
+# lambda functions:
+l_x = lambda x, y, z: x
+l_y = lambda x, y, z: y
+l_t = lambda x, y, z: z
+l_cos_pi = lambda x, y, z: math.cos(math.pi * x)
+l_sin_pi = lambda x, y, z: math.sin(math.pi * x)
+l_avg = lambda x, y, z: (x + y + z)/3
+l_prod = lambda x, y, z: (x*y*z)
+l_power = lambda x, y, z: x**2
+l_add_third = lambda x, y, z: (x+y+z)/3
+list_functions = [l_x, l_y, l_t, l_cos_pi, l_sin_pi,
+                  l_avg, l_prod, l_power, l_add_third]
 
-def build_random_function(min_depth, max_depth):
-    """ Builds a random function of depth at least min_depth and depth
-        at most max_depth (see assignment writeup for definition of depth
-        in this context)
 
-        min_depth: the minimum depth of the random function
-        max_depth: the maximum depth of the random function
-        returns: the randomly generated function represented as a nested list
-                 (see assignment writeup for details on the representation of
-                 these functions)
+def build_random_function(depth):
+    """ Builds a random function of a given depth
+        returns: nested list of lambda functions,
+         which take (x,y,z) input in generate_art()
     """
-    depth = random.randint(0, max_depth - min_depth)
-    min_depth = min_depth + depth
-    max_depth = min_depth
-    if min_depth == 0:
-        new_int = random.randint(0, 1)
-        if new_int == 0:
-            return ['x']
-        else:
-            return ['y']
-    picker = random.randint(0, 1)
-    if picker == 0:
-        single_int = random.randint(0, 2)
-        singles = ["cos_pi", "sin_pi", "power"]
-        return [singles[single_int],
-                build_random_function(min_depth - 1, max_depth - 1)]
+    # depth = random.randint(0, max_depth - min_depth)
+    # min_depth = min_depth + depth
+    # max_depth = min_depth
+    if depth <= 1:
+        new_int = random.randint(0, 2)
+        return list_functions[new_int]
+
     else:
-        double_int = random.randint(0, 2)
-        doubles = ["prod", "avg", "add_third"]
-        return [doubles[double_int],
-                build_random_function(min_depth - 1, max_depth - 1),
-                build_random_function(min_depth - 1, max_depth - 1)]
-
-
-def evaluate_random_function(f, x, y):
-    if f[0] == 'x':
-        return x
-    if f[0] == 'y':
-        return y
-    if f[0] == "cos_pi":
-        return math.cos(3.14 * evaluate_random_function(f[1], x, y))
-    if f[0] == "sin_pi":
-        return math.sin(3.14 * evaluate_random_function(f[1], x, y))
-    if f[0] == "avg":
-        fadd2 = evaluate_random_function(f[2], x, y)
-        return 0.5 * (evaluate_random_function(f[1], x, y) + fadd2)
-    if f[0] == "prod":
-        f2 = evaluate_random_function(f[2], x, y)
-        return evaluate_random_function(f[1], x, y) * f2
-    if f[0] == "power":
-        f1 = evaluate_random_function(f[1], x, y)
-        return f1 * f1
-    if f[0] == "add_third":
-        f1 = evaluate_random_function(f[1], x, y)
-        f2 = evaluate_random_function(f[2], x, y)
-        return (f1 + f2)/3
-    return 'None'
-
-    """ Evaluate the random function f with inputs x,y
-        Representation of the function f is defined in the assignment writeup
-
-        f: the function to evaluate
-        x: the value of x to be used to evaluate the function
-        y: the value of y to be used to evaluate the function
-        returns: the function value
-
-        >>> evaluate_random_function(["x"],-0.5, 0.75)
-        -0.5
-        >>> evaluate_random_function(["y"],0.1,0.02)
-        0.02
-    """
+        picker = random.randint(3, 8)
+        f1 = build_random_function(depth - 1)
+        f2 = build_random_function(depth - 1)
+        f3 = build_random_function(depth - 3)
+        return lambda x, y, t: list_functions[picker](f1(x, y, t),
+                                                      f2(x, y, t), f3(x, y, t))
 
 
 def remap_interval(val,
@@ -160,16 +122,17 @@ def test_image(filename, x_size=350, y_size=350):
     im.save(filename)
 
 
-def generate_art(filename, x_size=350, y_size=350):
+def generate_art(filename, x_size=350, y_size=350, z=70):
     """ Generate computational art and save as an image file.
 
         filename: string filename for image (should be .png)
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
     # Functions for red, green, and blue channels - where the magic happens!
-    red_function = build_random_function(7, 9)
-    green_function = build_random_function(2, 5)
-    blue_function = build_random_function(7, 10)
+    depth = 5
+    red_function = build_random_function(depth)
+    green_function = build_random_function(depth)
+    blue_function = build_random_function(depth)
 
     # Create image and loop over all pixels
     im = Image.new("RGB", (x_size, y_size))
@@ -179,9 +142,13 @@ def generate_art(filename, x_size=350, y_size=350):
             x = remap_interval(i, 0, x_size, -1, 1)
             y = remap_interval(j, 0, y_size, -1, 1)
             pixels[i, j] = (
-                    color_map(evaluate_random_function(red_function, x, y)),
-                    color_map(evaluate_random_function(green_function, x, y)),
-                    color_map(evaluate_random_function(blue_function, x, y))
+                    # functions are directly evaluated, instead of using evaluate_random_function()
+                    color_map(red_function(x, y, z)),
+                    color_map(green_function(x, y, z)),
+                    color_map(blue_function(x, y, z))
+                    # color_map(evaluate_random_function(red_function, x, y)),
+                    # color_map(evaluate_random_function(green_function, x, y))
+                    # color_map(evaluate_random_function(blue_function, x, y))
                     )
 
     im.save(filename)
@@ -194,7 +161,7 @@ if __name__ == '__main__':
     # Create some computational art!
     # TODO: Un-comment the generate_art function call after you
     #       implement remap_interval and evaluate_random_function
-    generate_art("myart1.png")
+    generate_art("myart_l_3.png")
 
     # Test that PIL is installed correctly
     # TODO: Comment or remove this function call after testing PIL install
